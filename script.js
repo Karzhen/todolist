07e7161481d899db6b72d430ae2d328d0102b04a
Черкашин Егор Александрович <cea@mns.mnsspb.ru>
2024-02-21 16:30:51 +0300

    feat: almost all logic has been added

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const newCard = document.getElementById("add-new-card");
+const searchInput = document.getElementById("search-note");
+
 let myModal,
     filters = document.querySelectorAll('input[type="radio"][name="filter"]'),
     currentCategory = document.querySelectorAll('input[type="checkbox"][name="category"]'),
-    selectedFilter = 'all';
+    selectedFilter = 'all',
+    selectedCategories = [];
 
 function addModal() {
     // Создаем элементы модального окна
@@ -80,6 +83,13 @@ function createTask() {
     const category = document.getElementById("noteCategory").value;
     const text = document.getElementById("noteText").value;
 
+    const note = {
+        title: title,
+        category: category,
+        text: text,
+        classes: []
+    };
+
     const card = document.createElement("li");
     card.classList.add("card");
 
@@ -98,7 +108,7 @@ function createTask() {
     deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" stroke="#FFF" stroke-width="1.5" d="M5 7H21M9 7V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v3M3 7l1 14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2l1-14M9 11h6"/></svg>';
     deleteButton.addEventListener('click', function() {
         card.classList.toggle("trash");
-        removeCard(card, selectedFilter);
+        removeCard(card, selectedFilter, title);
     });
 
     const favoriteButton = document.createElement("button");
@@ -106,6 +116,18 @@ function createTask() {
     favoriteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" stroke="#FFF" stroke-width="1.5" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>';
     favoriteButton.addEventListener('click', function() {
         card.classList.toggle("favorites");
+        removeCard(card, selectedFilter);
+        if (card.classList.contains("favorites")) {
+            const path = favoriteButton.querySelector('svg path');
+            if (path) {
+                path.style.fill = '#FFF';
+            }
+        } else {
+            const path = favoriteButton.querySelector('svg path');
+            if (path) {
+                path.style.fill = 'none';
+            }
+        }
     });
 
     actions.appendChild(deleteButton);
@@ -129,8 +151,11 @@ function createTask() {
     cardDescription.textContent = text;
 
     card.appendChild(cardDescription);
+    note.classes = Array.from(card.classList).toString();
+    console.log(note.classes)
 
     document.querySelector(".cards-list").appendChild(card);
+    saveNoteToLocalStorage(note);
 
     removeModal(myModal);
 }
@@ -158,7 +183,7 @@ filters.forEach(filter => {
 
 currentCategory.forEach(category => {
     category.addEventListener('change', function() {
-        const selectedCategories = [];
+        selectedCategories = [];
         document.querySelectorAll('input[name="category"]:checked').forEach(checkbox => {
             selectedCategories.push(checkbox.value);
         });
@@ -211,8 +236,12 @@ function openCategories(categories) {
     });
 }
 
-function removeCard(card, currentFilter) {
-    console.log(currentFilter);
+function removeCard(card, currentFilter, title) {
+    if (currentFilter === 'favorites') {
+        if (!card.classList.contains('favorites')) {
+            card.style.display = 'none';    
+        }
+    }
     if (currentFilter === 'all' || currentFilter === 'favorites') {
         if (card.classList.contains('trash')) {
             card.style.display = 'none';
@@ -220,7 +249,155 @@ function removeCard(card, currentFilter) {
     }
     if (currentFilter === 'trash') {
         if (!card.classList.contains('trash')) {
-            card.style.display = 'none';
+            removeNoteFromLocalStorage(title); 
+            card.remove();
         }
     }
 }
+
+searchInput.addEventListener('input', function(event) {
+    const searchText = event.target.value.toLowerCase();
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        let shouldDisplay = false;
+
+        if (card.querySelector('.card-title').textContent.toLowerCase().includes(searchText) || 
+            card.querySelector('.card-description').textContent.toLowerCase().includes(searchText)) {
+            shouldDisplay = true;
+
+            for (const category of selectedCategories) {
+                const categoryClass = 'card' + category.charAt(0).toUpperCase() + category.slice(1);
+                if (card.classList.contains(categoryClass)) {
+                    shouldDisplay = true;
+                    break;
+                } else {
+                    shouldDisplay = false;
+                }
+            }
+        } else {
+            shouldDisplay = false;
+        }
+
+        if (selectedFilter === 'all') {
+            if (card.classList.contains('trash')) {
+                shouldDisplay = false;
+            }
+        } else if (selectedFilter === 'trash') {
+            if (!card.classList.contains('trash')) {
+                shouldDisplay = false;
+            }
+        } else if (selectedFilter === 'favorites') {
+            if (!card.classList.contains('favorites')) {
+                shouldDisplay = false;
+            }
+        }
+
+        card.style.display = shouldDisplay ? 'list-item' : 'none';
+    });
+});
+
+function getNotesFromLocalStorage() {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    return notes ? notes : [];
+}
+
+function saveNoteToLocalStorage(note) {
+    let notes = getNotesFromLocalStorage();
+    console.log(`Before LS:`)
+    console.log(notes)
+    notes.push(note);
+    console.log(`After LS:`)
+    console.log(notes)
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function removeNoteFromLocalStorage(title) {
+    let notes = getNotesFromLocalStorage();
+    notes = notes.filter(note => note.title !== title);
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function loadNotesFromLocalStorage() {
+    const notes = getNotesFromLocalStorage();
+    console.log(notes.length)
+
+    if (notes.length > 0) {
+        console.log("abracaadbra_")
+        notes.forEach(note => {
+            const card = document.createElement("li");
+            card.classList.add("card");
+
+            const cardHeader = document.createElement("div");
+            cardHeader.classList.add("card-header");
+
+            const cardTitle = document.createElement("h2");
+            cardTitle.classList.add("card-title");
+            cardTitle.textContent = note.title;
+
+            const actions = document.createElement("div");
+            actions.classList.add("actions");
+
+            const deleteButton = document.createElement("button");
+            deleteButton.classList.add("delete-button");
+            deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" stroke="#FFF" stroke-width="1.5" d="M5 7H21M9 7V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v3M3 7l1 14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2l1-14M9 11h6"/></svg>';
+            deleteButton.addEventListener('click', function() {
+                card.classList.toggle("trash");
+                removeCard(card, selectedFilter, note.title);
+            });
+        
+            const favoriteButton = document.createElement("button");
+            favoriteButton.classList.add("favorite-button");
+            favoriteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" stroke="#FFF" stroke-width="1.5" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>';
+            favoriteButton.addEventListener('click', function() {
+                card.classList.toggle("favorites");
+                removeCard(card, selectedFilter);
+                if (card.classList.contains("favorites")) {
+                    const path = favoriteButton.querySelector('svg path');
+                    if (path) {
+                        path.style.fill = '#FFF';
+                    }
+                } else {
+                    const path = favoriteButton.querySelector('svg path');
+                    if (path) {
+                        path.style.fill = 'none';
+                    }
+                }
+            });
+        
+            actions.appendChild(deleteButton);
+            actions.appendChild(favoriteButton);
+            
+            cardHeader.appendChild(cardTitle);
+            cardHeader.appendChild(actions);
+        
+            card.appendChild(cardHeader);
+        
+            if (note.category === "shopping") {
+                card.classList.add('cardShopping');
+            } else if (note.category === "business") {
+                card.classList.add('cardBusiness');
+            } else if (note.category === "other") {
+                card.classList.add('cardOther');
+            }
+        
+            const cardDescription = document.createElement("p");
+            cardDescription.classList.add("card-description");
+            cardDescription.textContent = note.text;
+        
+            card.appendChild(cardDescription);
+        
+            document.querySelector(".cards-list").appendChild(card);
+        });
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('input[name="category"]:checked').forEach(checkbox => {
+        selectedCategories.push(checkbox.value);
+    });
+    loadNotesFromLocalStorage();
+})
